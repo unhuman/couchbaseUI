76a6548296d0fcc7cb4d5b151e54a749fbf15365
Uman, Howard <human@cvent.com>
2021-02-15 18:08:36 -0500

    Improve handling of TTL when type changes

--- src/main/java/com/unhuman/couchbaseui/CouchbaseUI.java
+++ src/main/java/com/unhuman/couchbaseui/CouchbaseUI.java
@@ -70,6 +70,8 @@ import java.awt.desktop.QuitHandler;
 import java.awt.desktop.QuitResponse;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
@@ -524,6 +526,16 @@ public class CouchbaseUI implements AboutHandler, QuitHandler {
             public void actionPerformed(ActionEvent e) {
                 config.setLogsWordWrap(checkBoxLogsWordWrap.isSelected());
                 textAreaLogs.setLineWrap(checkBoxLogsWordWrap.isSelected());
+            }
+        });
+        comboBoxTTLDurationType.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    // either set to zero or wipe out the TTL value
+                    textFieldTTLAmount.setText((comboBoxTTLDurationType.getSelectedItem().equals(NO_EXPIRY_STRING))
+                            ? "0" : "");
+                }
             }
         });
     }
